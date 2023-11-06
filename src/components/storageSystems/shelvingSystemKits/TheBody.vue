65d5974d7868e759b6d81970c28028f0925a7975
g.ptushenko <g.ptushenko@doapp.ru>
2023-11-06 22:16:46 +0300

    10-TT footer

--- src/components/storageSystems/shelvingSystemKits/TheBody.vue
+++ src/components/storageSystems/shelvingSystemKits/TheBody.vue
@@ -3,10 +3,18 @@ import {defineComponent} from "vue";
 import TheFilter  from "@/components/storageSystems/shelvingSystemKits/TheFilter.vue";
 import TheHeader  from "@/components/storageSystems/shelvingSystemKits/TheHeader.vue";
 import TheContent from "@/components/storageSystems/shelvingSystemKits/TheContent.vue";
+import TheFooter  from "@/components/storageSystems/shelvingSystemKits/TheFooter.vue";
 
 export default defineComponent({
   name: 'TheBody',
-  components: {TheContent, TheFilter, TheHeader},
+
+  components: {
+    TheFooter,
+    TheContent,
+    TheFilter,
+    TheHeader
+  },
+
 })
 </script>
 
@@ -14,6 +22,7 @@ export default defineComponent({
   <TheHeader title="Комплекты стеллажных систем"/>
   <TheFilter />
   <TheContent />
+  <TheFooter />
 </template>
 
 
---
+++ src/components/storageSystems/shelvingSystemKits/TheFooter.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'TheFooter',
+})
+</script>
+
+<template>
+  <footer class="footer" />
+</template>
+
+<style scoped>
+.footer {
+  width: 100%;
+  height: 100px;
+}
+</style>
