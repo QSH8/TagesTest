23f6be7b07f0f995504007c1c823113ab3a6cc32
g.ptushenko <g.ptushenko@doapp.ru>
2023-11-06 22:13:14 +0300

    7-TT Фильтр

---
+++ src/components/storageSystems/shelvingSystemKits/FilterItem.vue
@@ -0,0 +1,68 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'FilterItem',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    }
+  },
+
+
+})
+</script>
+
+<template>
+  <div class="filter__item">
+    <p class="filter__item-title">{{ title }}</p>
+    <select name="filter__item-select" id="filter__item-select">
+      <option :value="''"></option>
+      <option :value="'some'">some</option>
+      <option :value="'shit'">shit</option>
+    </select>
+  </div>
+</template>
+
+<style scoped>
+ .filter__item {
+   width: 288px;
+   padding-bottom: 18px;
+ }
+
+ .filter__item-title {
+   font-family: SF-ProDisplay-regular;
+   font-size: 12px;
+   color: #4F4F4F;
+   margin: 0 0 6px 16px;
+ }
+
+ #filter__item-select {
+   width: 100%;
+   height: 40px;
+   font-family: SF-ProDisplay-regular;
+   font-size: 14px;
+   line-height: 150%;
+   padding: 9px 0 10px 16px;
+   -moz-appearance: none;
+   -webkit-appearance: none;
+   appearance: none;
+   color: #000000;
+   background-color: #F2F2F2;
+   background-image: url("@/assets/icons/select_icon.svg");
+   background-repeat: no-repeat, repeat;
+   background-position: right 19.3px top 50%, 0 0;
+
+   border: none;
+ }
+
+ #filter__item-select::-ms-expand {
+  display: none;
+ }
+
+ #filter__item-select:focus {
+   outline: none;
+ }
+</style>
---
+++ src/components/storageSystems/shelvingSystemKits/TheFilter.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+import FilterItem from "@/components/storageSystems/shelvingSystemKits/FilterItem.vue";
+
+export default defineComponent({
+  name: 'TheFilter',
+  components: {FilterItem},
+
+})
+</script>
+
+<template>
+  <div class="filter">
+    <FilterItem
+      title="Сортировать по: "
+    />
+    <FilterItem
+      title="Материал: "
+    />
+  </div>
+</template>
+
+<style scoped>
+ .filter {
+   display: flex;
+   flex-direction: row;
+   justify-content: start;
+   align-items: center;
+   margin-bottom: 23px;
+   gap: 24px;
+
+ }
+</style>
