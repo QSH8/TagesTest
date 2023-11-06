c99ae68df47559553735b8916234af82588be963
g.ptushenko <g.ptushenko@doapp.ru>
2023-11-06 23:52:57 +0300

    11-TT Store 1. - Правки

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,38 @@
 import { createStore } from 'vuex'
 import products from "@/store/modules/products";
+import axios from "axios";
 
 export default createStore({
     modules: {
         products: products,
+    },
+    actions: {
+        /**
+         * Метод get-запроса на наш back-end
+         * @param context
+         * @param params
+         * @return {Promise<void>}
+         */
+        ajaxGet: async (context, params) => {
+            await axios.get(params)
+                .then(response => {
+                    return response
+                })
+                .catch(err => console.log(err))
+        },
+
+        /**
+         * Метод post-запроса на наш back-end
+         * @param context
+         * @param params
+         * @return {Promise<void>}
+         */
+        ajaxPost: async (context, params) => {
+            await axios.post(params)
+                .then(response => {
+                    return response
+                })
+                .catch(err => console.log(err))
+        }
     }
 })
