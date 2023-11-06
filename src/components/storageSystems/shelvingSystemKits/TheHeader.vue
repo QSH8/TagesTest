328da9682fd47642848cd6b4e1dc6e9f3bddab01
g.ptushenko <g.ptushenko@doapp.ru>
2023-11-06 22:13:58 +0300

    9-TT header

---
+++ src/components/storageSystems/shelvingSystemKits/TheHeader.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'TheHeader',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+
+})
+</script>
+
+<template>
+  <header class="header">
+    <p class="header__title">{{ title }}</p>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  padding: 40px 0 32px 0 ;
+}
+
+.header__title {
+  font-family: SF-ProDisplay-regular;
+  font-size: 36px;
+  font-weight: bold;
+  line-height: 48px;
+}
+</style>
